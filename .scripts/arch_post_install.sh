e61143e6b36b2901c5bd5f2d7207fa9e81233ae1
Abhishek Kaith <abhishekkaith76@gmail.com>
2026-01-24 02:25:58 +0530

    niri

--- .scripts/arch_post_install.sh
+++ .scripts/arch_post_install.sh
@@ -154,7 +154,7 @@ sudo ufw status verbose
 
 sudo pacman -Sy niri alacritty fuzzel xwayland-satellite xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-gnome  --needed --noconfirm
 
-yay -S noctalia-shell cliphist matugen cava wlsunset power-profiles-daemon --needed --noconfirm
+yay -S noctalia-shell-git cliphist cava wlsunset matugen python3 evolution-data-server power-profiles-daemon --needed --noconfirm
 
 sudo pacman -S keepassxc qt5-wayland --needed --noconfirm
 
@@ -162,7 +162,10 @@ sudo pacman -S speech-dispatcher libnotify hunspell-en_US festival espeak-ng --n
 
 sudo pacman -Syu nautilus evince mpv ffmpeg imagemagick gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gvfs-google gvfs-wsdd ffmpegthumbnailer poppler gdk-pixbuf2 librsvg libgepub libopenraw tumbler gthumb --needed --noconfirm
 
-sudo pacman -S flatpak adw-gtk-theme nwg-look qt6ct
+sudo pacman -S flatpak adw-gtk-theme nwg-look qt6ct --needed --noconfirm
+sudo pacman -S keepassxc --needed --noconfirm
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 flatpak install org.gtk.Gtk3theme.adw-gtk3-dark
 flatpak install org.gtk.Gtk3theme.adw-gtk3
 
